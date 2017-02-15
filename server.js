6d8545b9991bc5dffc2c94de99159230c175beed
Michael Blood <michaelblood@videobot.local>
2017-02-15 02:56:46 -0600

    timestamp functionality complete

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = Number(process.argv[2]) || 3000;
+const months = ['January', 'February', 'March', 'April',
+                'May', 'June', 'July', 'August',
+                'September', 'October', 'November', 'December'];
+
+const getUnix = (date) => Date.parse(date);
+const getNatural = (date) => {
+  const d = date.getDate()
+  const m = months[date.getMonth()];
+  const y = date.getFullYear();
+
+  return `${m} ${d}, ${y}`;
+};
+
+app.get('/', (req, res) => {
+  res.end('Timestamp');
+});
+
+app.get('/:datestring', (req, res) => {
+  try {
+    let datestring = req.params.datestring;
+    let date = new Date(datestring);
+    if (date.getDate() != date.getDate()) {
+      date = new Date(Number(datestring));
+    }
+    if (date.getDate() != date.getDate()) {
+      res.end({unix: null, natural: null});
+      return;
+    }
+    let out = {
+      unix: getUnix(date),
+      natural: getNatural(date)
+    };
+
+    res.end(JSON.stringify(out));
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
